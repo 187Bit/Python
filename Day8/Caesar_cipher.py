4959c0c23de675c8edcb0b063a2177a09c9e0168
187Bit <joelb.wk@outlook.com>
2022-08-24 11:40:49 +0200

    Started caesar_cypher project

---
+++ Day8/Caesar_cipher.py
@@ -0,0 +1,48 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(text, shift):
+    
+    new_text = []
+
+    for i in range (0, len(text)):
+
+        position_in_alphabet = 0
+
+        for l in range(0, len(alphabet)):
+
+            if text[i] == alphabet[l]:
+                position_in_alphabet = l
+                # print(position_in_alphabet)
+        
+
+        shift_value = position_in_alphabet + shift
+
+        if shift_value > len(alphabet):
+           shift_value =  shift_value - len(alphabet)
+
+        new_text.append(alphabet[shift_value])
+
+        print(new_text) # convert into string by using for loop or the corresponding function!!!!!!!!!!!
+
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+encrypt(text, shift)
