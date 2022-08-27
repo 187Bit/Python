125fa6fa9894f4b5ef1bb8cfc238a20cfedbabe1
187Bit <joelb.wk@outlook.com>
2022-08-27 12:02:19 +0200

    Added an exmaple code for the extend() function

---
+++ Day4/extend.py
@@ -0,0 +1,7 @@
+list = [] # Empty list
+
+list_2 = [1,2,3,4,5] # List with 5 value
+
+list.extend(list_2) # .extend() extends the list with another list instead of one value (.append())
+
+print(list)
